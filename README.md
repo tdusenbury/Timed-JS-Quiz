794f80473326a1cce8e00ea68e6a0b235419599b
The Duz <tamara.dusenbury@gmail.com>
2023-04-03 22:16:11 -0700

    more what not to do, but focusing on the learning experience

--- README.md
+++ README.md
@@ -7,16 +7,15 @@
 | Technology Used         | Resource URL           | 
 | ------------- |:-------------:| 
 | JavaScript | [https://www.w3schools.com/jsref/event_onclick.asp](https://www.w3schools.com/jsref/event_onclick.asp);  [https://www.w3schools.com/jsref/dom_obj_pushbutton.asp](https://www.w3schools.com/jsref/dom_obj_pushbutton.asp); [https://www.w3schools.com/jsref/prop_style_position.asp](https://www.w3schools.com/jsref/prop_style_position.asp); [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array);[https://www.w3schools.com/jsref/prop_win_localstorage.asp](https://www.w3schools.com/jsref/prop_win_localstorage.asp);
- 
-  [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random); [https://www.w3schools.com/js/js_objects.asp](https://www.w3schools.com/js/js_objects.asp); [https://www.w3schools.com/js/js_strings.asp](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor)
+[https://www.w3schools.com/js/tryit.asp?filename=tryjson_store](https://www.w3schools.com/js/tryit.asp?filename=tryjson_store); 
 
 ## Description 
 
-This application is a JavaScript Quiz. The Quiz has a start button that begins the quiz. A timer starts when the quiz begins. The next question appears once an answer is chosen. If a question is asked incorrectly, the timer is reduced by 15 seconds. The user plays until all questions are answered or the timer reaches zero. The user is then presented with their score and the option to enter their initials to be saved to the high score page. 
+This application is a JavaScript Quiz. The Quiz was to have a start button that begins the quiz. A timer starts when the quiz begins. The next question appears once an answer is chosen. If a question is answered incorrectly, the timer is reduced by 15 seconds. The user should play until all questions are answered or the timer reaches zero. The user is then presented with their score and the option to enter their initials to be saved to the high score page. 
 
 
 
-![Screenshot of My Site](images/PG%20Site.PNG)
+![Screenshot of My Site](images/screen%20pic.PNG)
 
 
 
@@ -26,46 +25,29 @@ This application is a JavaScript Quiz. The Quiz has a start button that begins t
 
 ## Code Example
 
-Instead of using the array method to state all of my elements, I chose to use the object method by defining "keys" and then pulling each of the chosen elements into a new string called customerChoices.
+I had a lot of issues with this code. I finally was able to populate all the fields separately, but I did a total git failure and didn't push up. I was unable to get back to where I was and each section now overlaps one or the other.
 
-```
-Created an object:
-const keys = {
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  numbers: "0123456789",
-  symbols: "!@#$%^&*~+={}[]:,./"
-}
-
-The new string:
-var customerChoices =""
-
-Directed choices to go into the new string:
-if (confirmLowerCase === true) {
-    console.log("Before ", customerChoices);
-    customerChoices = customerChoices + keys.lowerCase;
-    console.log("After ", customerChoices);
-}
-```
+![Screenshot of My Site](images/start%20quiz.PNG)
 
 
 ## Usage 
 
-This Password Generator can be used by anyone who wants to control most of the length and types of characters in a random password.
+When up and running, this would be a great tool for creating study questions for review as they were studying each night. You could create one for each applicable class. With more skills, the questions could be randomized to ensure greatest challenge.
 
 
 ## Learning Points 
 
-This project involved utilizing JavaScript. I tried to approach it section by section initially, but became stuck. I was able to get advice from my tutor as well as classmates and went back to writing each section followed by a console.log to test that the code was working without error. I learned about arrays and objects, random number/character generation, the types of message boxes prompts, alerts, and confirms can produce.
+This was another project that utilized JavaScript. I learned from my last project to approach each part section by section. I did start off really well and was remembering to git commit regularly.  I created an array that initially loaded to my quiz screen and rotated throught the questions. I then added the high score page and tried to complete the hidden/visible styles on each section. Somewhere in there I both forgot to git commit and I tried to do too many things at once and lost control.
 
-I struggled with this project every time I tried to move to the next part before it was ready. It took lots of testing before I was able to get my "If not truthy" alert correct if someone did not choose any options. I was trying to evaluate it as an equality not as a true/false statement.
-Fortunately, a great classmate helped explain my error and helped me simplify the statement.
-Here are pictures of the code that finally worked and the message it now produces if no character options are chosen:
+Frankly, I thought I had it because I did understand how to create each part, but I got cocky and forgot to follow best practices.
 
-![Screenshot of My Site](images/Code%20if%20No%20Choices.PNG)
+At the same time, I have to remind myself that just over two weeks ago I had zero experience with this kind of work. This application may not actually do everything that it is supposed to, but what I did create is not far from being useful. 
 
+I look forward to learning more and then coming back to this when the cohort is over and debugging and refactoring my work.
 
-![Screenshot of My Site](images/No%20Choices%20Made%20Message.PNG)
+I am also learning that I really enjoy the styling aspect of this work.
+
+PS This will forever be the project I was working on when my son completed his Eagle Project. 
 
 ## Author Info
 
@@ -83,7 +65,7 @@ TAMARA "T" DUSENBURY
 
 ## Credits
 
-Shout out to Laurie Fish for helping keep the ideas rolling and weird for more hours than we care to admit; to my tutor Katie; and to Liubov Sobolevskaya for making me think so very hard!!!
+Shout out to Laurie and Tia for being moral support and extra eyes!!
 
 
 ## License
--- assets/script.js
+++ assets/script.js
@@ -1,125 +1,141 @@
 var quizQuestionsArray = [
     {
-        question: "Conditional statements are used to perform different actions based on different conditions and typically include the following term(s) or phrase:",
+        questionText: "Conditional statements are used to perform different actions based on different conditions and typically include the following term(s) or phrase:",
         answers: ["The symbols less than < and greater than >", "Hoisted by your own petard", "If, else, and/or switch", "Stringify"],
-        correct: "If, else, and/or switch"
+        correctAnswer: "If, else, and/or switch",
     },
 
     {
-        question: "Data types like undefined, string, number, and Boolean are called:",
-        answers: ["True / False Data Types", "Array Data Types", "For Loops", "Primitive Data Types"]
-        correct: "Primitive Data Types"
+        questionText: "Data types like undefined, string, number, and Boolean are called:",
+        answers: ["True / False Data Types", "Array Data Types", "For Loops", "Primitive Data Types"],
+        correctAnswer: "Primitive Data Types",
     },
 
     {
-        question: "An object in JavaScript can have:",
-        answers: ["Only Text Values", "Only Numerical Values", "Many Values", "Imaginary Values"]
-        correct: "Many Values"
+        questionText: "An object in JavaScript can have:",
+        answers: ["Only Text Values", "Only Numerical Values", "Many Values", "Imaginary Values"],
+        correctAnswer: "Many Values",
     },
 
     {
-        question: "A JavaScript string, a way to store and manipulate text, contain zero or more characters written inside:",
-        answers: ["Curly Brackets", "Equal Signs", "Square Parenthesis", "Quotation Marks"]
-        answer: "Quotation Marks"
+        questionText: "A JavaScript string, a way to store and manipulate text, contain zero or more characters written inside:",
+        answers: ["Curly Brackets", "Equal Signs", "Square Parenthesis", "Quotation Marks"],
+        correctAnswer: "Quotation Marks",
     },
 
     {
-        question: "To remove the last element from an array in JavaScript, you use which method?",
-        answers: ["The Method Process","methodActing()", "The pop() Method","The string() Method"]
-        answer: "The pop() Method"
+        questionText: "To remove the last element from an array in JavaScript, you use which method?",
+        answers: ["The Method Process","methodActing()", "The pop() Method","The string() Method"],
+        correctAnswer: "The pop() Method",
     }
 ];
 
-//defining elements between html, css, and js start screen
-const scoreButton =document.getElementById("#score-button");
-const returnHomeButton = document.getElementById("#return-home-button")
-const homePage = document.getElementById("start-box")
-const startButton = document.getElementById("start-button");
-const question = document.getElementById("question");
-const quiz = document.getElementById("inside-quiz-container")
-const scoreRepot = document.getElementById("score-report");
+const returnHomeBtn = document.getElementById("return-home-button");
+const resetScoreBtn = document.getElementById("reset-button");
 const scoreList = document.getElementById("score-list");
-const aQuestion = document.getElementById("a_question");
-const bQuestion = document.getElementById("b_question");
-const cQuestion = document.getElementById("c_question");
-const dQuestion = document.getElementById("d_question");
-const nextButton = document.getElementById("ques-next-btn");
+const finalScorePage = document.getElementById("top-score-container");
+const scoreData = document.querySelector(".score-data")
+var startButton = document.getElementById("start-button");
+const scoreButton =document.getElementById("score-button");
+const homePage = document.getElementById("start-box");
+const answerCheck = document.getElementById("winner-loser");
+const finalAnswer = document.getElementById("finalAnswer");
+const question = document.getElementById("question");
+const questionList = document.getElementById("question-list");
+const quizPage = document.getElementById("quiz-container");
+var timerEl = document.getElementById("timer");
+const initialEntry = document.getElementById("score-list");
+var scoreReportCard = document.getElementById("score-report-container");
+startButton.addEventListener('click', startQuiz);
+scoreButton.addEventListener("click", scoreReportPage);
+resetScoreBtn.addEventListener("click", resetScore);
+returnHomeBtn.addEventListener("click", homePage);
+const initialsBtn = document.getElementById("final-score-initials-button");
+var initials = document.getElementById("initials");
+const finalScore = document.getElementById("final-score");
 
 
-startButton.addEventListener("click", startQuiz);
-scoreButton.addEventListener("click", scoreReport);
-resetButton.addEventListener("click", resetScores);
-returnHomeButton.addEventListener("return-home-button", resetHome)
+var playerScore = 0;
 
-
-var playerScore = 0
-
-function scoreListSee() {
-    homePage.hidden = true;
-    quiz.hidden = true;
-    scoreReport.hidden = true;
-    returnHomeButton.hidden = false;
-    scoreButton.hidden = true;
-    //timer.hidden = true;  (UNGREEN)
+function scoreReportPage() {
+    finalScore.textContent = "Final score: " + finalScore;
+    homePage.style.visibility = "hidden";
+    quizPage.style.visibility = "hidden";
+    initialEntry.style.visibility = "hidden";
+    scoreReportCard.style.display = "flex";
+    returnHomeBtn.style.display = "flex";
+    scoreButton.style.visibility = "hidden";
+    timer.style.visibility = "hidden";
+    window.location.reload();
 }
 
-function resetScores() {
+initialsBtn.addEventListener("click", scoreReportPage); 
+
+function displayFinalScore () {
+    localStorage.setItem(inputInitials, value, score);
+};
+
+
+function resetScore() {
     scoreList.hidden = true;
     window.localStorage.clear();
 }
 
-var questionIndex = 0;
-function fillQuizBox() {
-    if (questionIndex >= quizQuestionsArray.length) {recordScore();}
-    newQuestion = quizQuestionsArray[questionIndex];
-    question.textContent = newQuestion.question;
-    a_question.textContent = newQuestion.answers[0]
-    b_question.textContent = newQuestion.answers[1]
-    c_question.textContent = newQuestion.answers[2]
-    d_question.textContent = newQuestion.answers[3]
+function startQuiz() {
+    homePage.style.visibility = "hidden";
+    quizPage.style.display = "flex";
+    scoreReportCard.style.visibility = "hidden";
+    returnHomeBtn.style.visibility = "hidden";
+    scoreButton.style.display = "flex";
+    startTimer();
+    fillQuizBox();
 }
 
-
-
-/*let quizStart = 0;
-let score = 0;
-
-function startQuiz() { 
-    const newQuizData = quizQuestions[quizStart];
-    const seeQuiz = document.getElementById("inside-quiz-container");
-    const removeStartBox = document.getElementById("start-container");
-    seeQuiz.style.display = "flex";
-    removeStartBox.style.visibility = "hidden";
-    question.innerText = newQuizData.question
-    aQuestion.innerText = newQuizData.a
-    bQuestion.innerText = newQuizData.b
-    cQuestion.innerText = newQuizData.c
-    dQuestion.innerText = newQuizData.d
-}
-function findAnswer() {
-    var answer = "";
-    const checkAnswer = document.getElementsByClassName("answer");
-    for(var i = 0; i < checkAnswer.length; i++) {
-        if(checkAnswer[i].checked) {
-            answer = checkAnswer[i].value;
-            console.log(answer);
-            if(answer === quizQuestions[quizStart].answer) {
-                quizStart++
-                score++
-            }else {
-                quizStart++;
-                alert ("That is the wrong answer.");
+var checkAnswerButton = document.querySelectorAll(".checkAnswerButton");
+    for(x = 0; x <checkAnswerButton.length; x++){
+        checkAnswerButton[x].addEventListener("click", function(event) {
+            if (event.target.innerText !== quizQuestionsArray[questionIndex].correctAnswer) {
+                answerStatus.textContent = "Incorrect";
             }
-        }   
+            if (event.target.innerText == quizQuestionsArray[questionIndex].correctAnswer) {
+                answerStatus.textContent = "Correct"
+            }
+            questionIndex++
+            fillQuizBox()
+        });
     }
-clearAnswers();
-startQuiz();
-}
-function clearAnswers() {
-        const checkAnswer = document.getElementsByClassName("answer");
-        for(var i = 0; i < checkAnswer.length; i++) {
-            checkAnswer[i].checked = false;
-        }
-}
-*/
 
+    var questionIndex = 0;
+    function fillQuizBox() {
+        if (questionIndex >= quizQuestionsArray.length) {recordScore();}
+        newQuestion = quizQuestionsArray[questionIndex];
+        question.innerText = newQuestion.questionText;
+        a_question.innerText = newQuestion.answers[0]
+        b_question.innerText = newQuestion.answers[1]
+        c_question.innerText = newQuestion.answers[2]
+        d_question.innerText = newQuestion.answers[3]
+    }
+    var secCount = 30;
+    let counter;
+
+    function startTimer() {
+        counter =setInterval(timer,1000);
+        function timer() {
+            secCount--;
+            timerEl.innerText = "" + secCount;
+            if(secCount == 0) {
+                clearInterval(counter);
+                resetScore();
+            }
+        }
+    }
+
+function resetHome() {
+    homePage.style.display = "flex";
+    quiz.style.visibility = "hidden";
+    scoreList.style.visibility = "hidden";
+    scoreReportCard.style.visibility = "hidden";
+    returnHomeButton.style.visibility = "hidden";
+    scoreButton.style.display = "flex";
+    window.location.reload();
+}

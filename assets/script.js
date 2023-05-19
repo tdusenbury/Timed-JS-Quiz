c5d226d59acd9ad182304013d7e0d01b2b751f99
The Duz <tamara.dusenbury@gmail.com>
2023-05-18 19:49:49 -0700

    debug timer; debug quiz questions; clean up code

--- assets/script.js
+++ assets/script.js
@@ -32,110 +32,160 @@ var quizQuestionsArray = [
 
 const returnHomeBtn = document.getElementById("return-home-button");
 const resetScoreBtn = document.getElementById("reset-button");
-const scoreList = document.getElementById("score-list");
-const finalScorePage = document.getElementById("top-score-container");
-const scoreData = document.querySelector(".score-data")
+const initialEntry = document.getElementById("score-list");
+//const finalScorePage = document.getElementById("top-score-container");
+// const scoreData = document.querySelector(".score-data")
 var startButton = document.getElementById("start-button");
 const scoreButton =document.getElementById("score-button");
 const homePage = document.getElementById("start-box");
 const answerCheck = document.getElementById("winner-loser");
-const finalAnswer = document.getElementById("finalAnswer");
+// const finalAnswer = document.getElementById("finalAnswer");
 const question = document.getElementById("question");
-const questionList = document.getElementById("question-list");
+//const questionList = document.getElementById("question-list");
 const quizPage = document.getElementById("quiz-container");
 var timerEl = document.getElementById("timer");
-const initialEntry = document.getElementById("score-list");
-var scoreReportCard = document.getElementById("score-report-container");
+var scoreReportContainer = document.getElementById("score-report-container");
+const initialsBtn = document.getElementById("final-score-initials-button");
+const initials = document.getElementById("initials");
+const finalScore = document.getElementById("final-score");
+const message = document.getElementById("answer");
+
+var playerScore = 0;
+var questionIndex = 0;
+var secCount = 60;
+let counter;
+var correctAnswer;
+
 startButton.addEventListener('click', startQuiz);
 scoreButton.addEventListener("click", scoreReportPage);
 resetScoreBtn.addEventListener("click", resetScore);
-returnHomeBtn.addEventListener("click", homePage);
-const initialsBtn = document.getElementById("final-score-initials-button");
-var initials = document.getElementById("initials");
-const finalScore = document.getElementById("final-score");
+returnHomeBtn.addEventListener("click", showStartPage);
+initialsBtn.addEventListener("click", displayFinalScore);
 
+const quizContainer = document.getElementById("inside-quiz-container");
+quizContainer.addEventListener('click', checkAnswerButton)
 
-var playerScore = 0;
+function checkAnswerButton(event) {
+  const choice = event.target;
+  if (choice.matches("p")){
+    if (choice.textContent === correctAnswer) {
+      message.textContent = "Correct Answer!";
+    } else {
+      message.textContent = "Wrong! You lose 15 seconds!";
+      secCount -=15;
+    }
+    questionIndex++;
+    setTimeout(fillQuizBox, 1000);
+  }
+}
 
 function scoreReportPage() {
-    finalScore.textContent = "Final score: " + finalScore;
+    scoreReportContainer.style.display = "block";
+    finalScore.textContent = "Final score: " + finalScoreValue;
     homePage.style.visibility = "hidden";
     quizPage.style.visibility = "hidden";
     initialEntry.style.visibility = "hidden";
-    scoreReportCard.style.display = "flex";
     returnHomeBtn.style.display = "flex";
     scoreButton.style.visibility = "hidden";
     timer.style.visibility = "hidden";
-    window.location.reload();
+    // window.location.reload();
 }
 
-initialsBtn.addEventListener("click", scoreReportPage); 
-
-function displayFinalScore () {
-    localStorage.setItem(inputInitials, value, score);
-};
-
-
+function displayFinalScore() {
+    playerScore = finalScore.textContent;
+    localStorage.setItem(initials.value, playerScore);
+  }
+  
 function resetScore() {
-    scoreList.hidden = true;
+    initialEntry.hidden = true;
     window.localStorage.clear();
 }
 
-function startQuiz() {
+function fillQuizBox() {
+    if (questionIndex >= quizQuestionsArray.length) {
+      recordScore();
+      return;
+    }
+    var newQuestion = quizQuestionsArray[questionIndex];
+    question.innerText = newQuestion.questionText;
+    correctAnswer = newQuestion.correctAnswer;
+    a_question.innerText = newQuestion.answers[0];
+    b_question.innerText = newQuestion.answers[1];
+    c_question.innerText = newQuestion.answers[2];
+    d_question.innerText = newQuestion.answers[3];
+  }
+   
+function startTimer() {
+    counter =setInterval(timer,1000);
+
+function timer() {
+    secCount--;
+    timerEl.innerText = "" + secCount;
+        if(secCount == 0) {
+            clearInterval(counter);
+            resetScore();
+        }
+    }
+}
+
+function recordScore() {
+  clearInterval(counter);
+    if(secCount > 0 && questionIndex >= quizQuestionsArray.length) {
+      answerCheck.textContent=`Well Done! Your Final Score Is: ${secCount}`;
+      finalScoreValue = secCount;
+    } else {
+      answerCheck.textContent=`No Good! Try Again!!`;
+    }
+}
+
+function resetHome() {
+    homePage.style.display = "none";
+    quizPage.style.visibility = "hidden";
+    scoreList.style.visibility = "hidden";
+    scoreReportContainer.style.visibility = "hidden";
+    returnHomeBtn.style.visibility = "hidden";
+    scoreButton.style.display = "flex";
+    window.location.reload();
+}
+
+// Define variables for each page/screen
+ let startPage = document.getElementById("start-container");
+// const scoreReportPage = document.getElementById("score-report-container");
+const topScorePage = document.getElementById("top-score-container");
+
+// Define functions to show and hide each page/screen
+function showStartPage() {
+  startPage.style.display = "block";
+  quizPage.style.display = "none";
+  scoreReportPage.style.display = "none";
+  topScorePage.style.display = "none";
+}
+
+function showQuizPage() {
+  startPage.style.display = "none";
+  quizPage.style.display = "block";
+  scoreReportPage.style.display = "none";
+  topScorePage.style.display = "none";
+}
+
+function showScoreReportPage() {
+  startPage.style.display = "none";
+  quizPage.style.display = "none";
+  scoreReportPage.style.display = "block";
+  topScorePage.style.display = "none";
+}
+
+  function startQuiz() {
+    showQuizPage
+    var timeLeft = 60;
+    timerEl.textContent = timeLeft;
     homePage.style.visibility = "hidden";
     quizPage.style.display = "flex";
-    scoreReportCard.style.visibility = "hidden";
+    scoreReportContainer.style.visibility = "hidden";
     returnHomeBtn.style.visibility = "hidden";
     scoreButton.style.display = "flex";
     startTimer();
     fillQuizBox();
 }
 
-var checkAnswerButton = document.querySelectorAll(".checkAnswerButton");
-    for(x = 0; x <checkAnswerButton.length; x++){
-        checkAnswerButton[x].addEventListener("click", function(event) {
-            if (event.target.innerText !== quizQuestionsArray[questionIndex].correctAnswer) {
-                answerStatus.textContent = "Incorrect";
-            }
-            if (event.target.innerText == quizQuestionsArray[questionIndex].correctAnswer) {
-                answerStatus.textContent = "Correct"
-            }
-            questionIndex++
-            fillQuizBox()
-        });
-    }
-
-    var questionIndex = 0;
-    function fillQuizBox() {
-        if (questionIndex >= quizQuestionsArray.length) {recordScore();}
-        newQuestion = quizQuestionsArray[questionIndex];
-        question.innerText = newQuestion.questionText;
-        a_question.innerText = newQuestion.answers[0]
-        b_question.innerText = newQuestion.answers[1]
-        c_question.innerText = newQuestion.answers[2]
-        d_question.innerText = newQuestion.answers[3]
-    }
-    var secCount = 30;
-    let counter;
-
-    function startTimer() {
-        counter =setInterval(timer,1000);
-        function timer() {
-            secCount--;
-            timerEl.innerText = "" + secCount;
-            if(secCount == 0) {
-                clearInterval(counter);
-                resetScore();
-            }
-        }
-    }
-
-function resetHome() {
-    homePage.style.display = "flex";
-    quiz.style.visibility = "hidden";
-    scoreList.style.visibility = "hidden";
-    scoreReportCard.style.visibility = "hidden";
-    returnHomeButton.style.visibility = "hidden";
-    scoreButton.style.display = "flex";
-    window.location.reload();
-}
+  
